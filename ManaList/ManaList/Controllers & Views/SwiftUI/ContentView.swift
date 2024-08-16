dd6957d0bc6e328b0f266f10ad38709e32f9b9a9
Konstantin Kolosov <rayleefx@protonmail.com>
2024-08-16 23:31:11 +0300

    Created and base integration

---
+++ ManaList/ManaList/Controllers & Views/SwiftUI/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  ManaList
+//
+//  Created by Konstantin Kolosov on 16.08.2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct DashboardFeature {
+
+//    @ObservableState
+//    struct State: Equatable {
+//        var counter = 50
+//    }
+    typealias State = DashboardState
+    typealias Action = DashboardAction
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .increaseCounter:
+                state.counter = min(100, state.counter + 5)
+                return .none
+            case .decreaseCounter:
+                state.counter = max(0, state.counter - 5)
+                return .none
+            }
+        }
+    }
+
+}
+
+@ObservableState
+struct DashboardState: Equatable {
+    var counter = 50
+}
+
+enum DashboardAction: Equatable {
+    case increaseCounter
+    case decreaseCounter
+}
+
+struct DashboardView: View {
+    let store: StoreOf<DashboardFeature>
+
+    var body: some View {
+        VStack {
+            HStack {
+                Button("-") {
+                    store.send(.decreaseCounter)
+                }
+                ZStack {
+                    let finishValue: CGFloat = min(1, CGFloat(store.counter) / 100)
+                    VStack {
+                        Text("\(store.counter)")
+                    }
+                    Circle()
+                        .trim(from: 0, to: finishValue)
+                        .stroke(lineWidth: 1.5)
+                        .frame(width: 70, height: 70)
+                        .rotationEffect(.degrees(-90))
+                        .animation(.snappy, value: finishValue)
+                }
+                Button("+") {
+                    store.send(.increaseCounter)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DashboardView(store: .init(initialState: DashboardFeature.State(), reducer: {
+        DashboardFeature()
+    }))
+}
---
+++ ManaList/ManaList/Supporting Files/ManaListApp.swift
@@ -0,0 +1,18 @@
+//
+//  ManaListApp.swift
+//  ManaList
+//
+//  Created by Konstantin Kolosov on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ManaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            DashboardView()
+            Text("Hello, World!")
+        }
+    }
+}
